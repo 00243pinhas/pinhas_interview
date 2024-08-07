92a1acf2a3f689d7cf7b7fde6ff10058831ab987
00243pinhas <pinaslok@gmail.com>
2024-08-07 10:46:00 +0300

    table population + search funvtionality

---
+++ script.js
@@ -0,0 +1,52 @@
+function fetchData() {
+    fetch('/index.php')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // console.log(data); 
+
+            populating(data);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function populating(datas){
+    
+    const tableOfTask = document.getElementById("bodyTable");
+
+    tableOfTask.innerHTML="";
+    
+    datas.forEach(function(data){
+
+        let row = document.createElement("tr");
+
+        row.innerHTML= `
+            <td>${data.task}</td>
+            <td>${data.title}</td>
+            <td>${data.description}</td>
+            <td class="color_code">${data.colorCode}</td>
+        `
+        // console.log(row)
+
+        tableOfTask.appendChild(row);
+
+
+        const colorBackgound = document.querySelectorAll(".color_code");
+
+        colorBackgound.forEach(function(color){
+        
+            let actualColor = color.innerHTML ;
+        
+            color.style.background = actualColor;
+        })
+        
+    })  
+
+
+}
+
+fetchData();
